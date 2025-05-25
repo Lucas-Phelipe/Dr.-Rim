e864ddc16d45732ce7ba747c67baa7d074e76cbd
Lucas Phelipe <lucasphelipe280@gmail.com>
2025-05-24 23:05:50 -0300

    update comunidade

--- src/pages/Comunidade/Comunidade.jsx
+++ src/pages/Comunidade/Comunidade.jsx
@@ -6,7 +6,7 @@ import styles from "./Comunidade.module.css";
 import { useNavigate } from "react-router-dom"; // Adicionado
 
 const Comunidade = () => {
-  const navigate = useNavigate(); // Adicionado
+  const navigate = useNavigate();
 
   const handleGoHome = () => {
     navigate("/");
@@ -15,39 +15,33 @@ const Comunidade = () => {
   return (
     <div className={styles.pageBackground}>
       <header className={styles.header}>
-        <h2>Dúvidas</h2>
+        <h2>Comunidade</h2>
         <img className={styles.profilePic} src="https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg" alt="Perfil" />
       </header>
 
       <div className={styles.topicSection}>
-        
-    
-          <h2 className={styles.topicTitle}>Comunidade</h2>
-            <h2 className={styles.topicText}>Interaja com os demais usuários, compartilhando experiências, conquistas, dúvidas e muito mais!</h2>
-        
+        <h2 className={styles.topicTitle}>Comunidade</h2>
+        <h2 className={styles.topicText}>Interaja com os demais usuários, compartilhando experiências, conquistas, dúvidas e muito mais!</h2>
       </div>
-        <div className={styles.cardsContainer}>
-          <div className={styles.cardPages} style={{ cursor: 'pointer' }} onClick={() => navigate('/quiz')}>
-            <div className={styles.containerContent}>
-              <span className={styles.containerTitle}>Galeria de conquistas</span>
-              <div className={styles.introImages}>
-            <img
-              src={RimIcon}
-              alt="Rim"
-              className={styles.introIcon}
-            />
+
+      <div className={styles.cardsContainer}>
+        <div className={styles.cardPages} style={{ cursor: 'pointer' }} onClick={() => navigate('/')}>
+          <div className={styles.containerContent}>
+            <span className={styles.containerTitle}>Galeria de conquistas</span>
+            <div className={styles.introImages}>
+              <img src={RimIcon} alt="Rim" className={styles.introIcon}/>
             </div>
           </div>
-          </div>
-
-           <div className={styles.cardsContainer}>
-          <div className={styles.cardPages} style={{ cursor: 'pointer' }} onClick={() => navigate('/quiz')}>
+        </div>
+        
+        <div className={styles.cardsContainer}>
+          <div className={styles.cardPages2} style={{ cursor: 'pointer' }} onClick={() => navigate('/')}>
             <div className={styles.containerContent}>
               <span className={styles.containerTitle}>Fórum</span>
             </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
       <Homebar />
     </div>
   );
